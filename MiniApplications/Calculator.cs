652164a49009a6a51696994b0c28e29a44e9c000
dennissulejman <45164504+dennissulejman@users.noreply.github.com>
2020-02-09 21:41:07 +0100

    Calculator implemented.

--- MiniApplications/Calculator.cs
+++ MiniApplications/Calculator.cs
@@ -1,24 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Threading.Tasks;
 
 namespace MultiAppRevamped.MiniApplications
 {
     internal class Calculator : IMiniApplication
     {
+        private Task<decimal> result;
+
+        public Calculator()
+        {
+            StartApplication();
+        }
+
         public void ReturnToMainMenu()
         {
-            throw new NotImplementedException();
+            MainMenu.Display();
         }
 
         public void StartApplication()
         {
-            throw new NotImplementedException();
+            WriteWelcomeMessage();
+            Continue();
         }
 
         public void WriteWelcomeMessage()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            Console.WriteLine("Write any mathematic formula using +, -, /, or * between the numbers");
+        }
+
+        private void Continue()
+        {
+            AcceptUserInput();
+            PrintResult();
+            CalculationFinishedPrompt();
+        }
+
+        private void AcceptUserInput()
+        {
+            result = Calculate(Console.ReadLine());
+        }
+
+        private async Task<decimal> Calculate(string formula)
+        {
+            return await CSharpScript.EvaluateAsync<decimal>(formula);
+        }
+
+        private async void PrintResult()
+        {
+            try
+            {
+                Console.WriteLine($"The result of the calculation is {await result}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not calculate the given expression, try again!");
+                Console.ReadLine();
+                StartApplication();
+            }
+        }
+
+        private void CalculationFinishedPrompt()
+        {
+            Console.WriteLine("Would you like to calculate a new problem? (y/n)");
+
+            switch (Console.ReadKey().KeyChar)
+            {
+                case 'y': StartApplication(); break;
+                case 'n': ReturnToMainMenu(); break;
+                default: CalculationFinishedPrompt(); break;
+            }
         }
     }
 }
